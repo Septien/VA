9c6ad36b8d6a414acda6fa8b5f33557f770f04a7
Jose Septien <jaseptienh@gmail.com>
2018-05-30 00:09:36 -0500

    Add streaming to data iterator.

--- dataIterator.py
+++ dataIterator.py
@@ -3,8 +3,13 @@ A module that implements an iterator for retrieving data from either a database 
 a .cvs file (posible a stream). The iterator abstracts the need for a generator 
 for each kind of data source, and handles all differences.
 """
-
+import wx
 import pymysql.cursors
+import threading
+import queue
+import time as t
+# For the streaming
+import dataStreaming as dS
 
 class Data(object):
     """
@@ -12,7 +17,7 @@ class Data(object):
     """
     def __init__(self, flag):
         """ Constructor of the class.
-            -flag: Indicates if a database or an csv is to be loaded (0 -> database, 1 -> csv).
+            -flag: Indicates if a database or an csv is to be loaded (0 -> database, 1 -> csv, 2 -> stream).
         """
         self.sourceFlag = flag
         self.dbCursor = None
@@ -20,6 +25,16 @@ class Data(object):
         self.File = None
         self.data = None
         self.name = None
+        # For the streaming
+        self.stream = None
+        self.exitQ = None
+        self.exitQLock = None
+        self.workQueue = None
+        self.workQLock = None
+        self.numVar = None
+        self.variables = None
+        self.thread = None
+
         self.length = 0
 
     def loadDB(self, host="", user="", passwd="", dbName=""):
@@ -54,6 +69,34 @@ class Data(object):
         # Open
         self.File = open(self.name, 'r')
 
+    def connectToStream(self, address, ctype):
+        """ 
+        Connects to a stream.
+            -address: (host, port) to connect to/from
+            -type: If the object will work as server or client
+        """
+        # For communicating between processes
+        self.workQueue = queue.Queue(1000)
+        self.exitQ = queue.Queue(1)
+        self.workQLock = threading.Lock()
+        self.exitQLock = threading.Lock()
+        self.stream = dS.Streaming(ctype)
+
+        # connect
+        if ctype == 0:
+            # As server
+            self.stream.recvConnections(address[1])
+        elif ctype == 1:
+            # As client
+            self.stream.connectToServer(address)
+
+        # Get description
+        self.numVar, self.variables = self.stream.recvVariablesDescr()
+        # Create listening thread
+        self.thread = dS.StreamingThread(1, "Thread1", self.workQueue, self.workQLock, 
+            self.stream, self.exitQ, self.exitQLock)
+        self.thread.start()
+
     def getDBDescription(self):
         """ Returns the description of the database:
             -The types of the variables.
@@ -71,8 +114,9 @@ class Data(object):
         #
         labels = []
         category = []
+
+        # If the source is a database
         if self.sourceFlag == 0:
-            # If the source is a database
             if not self.dbCursor:
                 return
 
@@ -93,8 +137,8 @@ class Data(object):
                 # Type
                 category.append(isNumeric(variable[1]))
 
+        # If the source is a csv.
         elif self.sourceFlag == 1:
-            # If the source is a csv.
             # Get the variables name of the csv
             line = self.File.readline()
             row = line.split(',')
@@ -104,6 +148,13 @@ class Data(object):
             row = line.split(',')
             for r in row:
                 category.append(int(r))
+        
+        # If the source is a stream
+        elif self.sourceFlag == 2:
+            labels = self.variables
+            self.length = self.numVar
+            for i in range(self.numVar):
+                category.append(0)
         
         self.length = len(labels)
         assert len(labels) == len(category), "Incorrect number of labels and category"
@@ -159,6 +210,33 @@ class Data(object):
                 # Read next row
                 if not noisy:
                     break
+        
+        elif self.sourceFlag == 2:
+            data = []
+            i = 0
+            while i < 10:
+                if self.exitQLock.acquire(blocking=False):
+                    # Check if there are some errors:
+                    if not self.exitQ.empty():
+                        style = wx.OK | wx.CENTER | wx.ICON_INFORMATION
+                        result = wx.MessageBox("Connection with client closed", "Connection closed", style)
+                        self.exitQLock.release()
+                        raise StopIteration()
+                if not workQLock.acquire(blocking=False):
+                    i += 1
+                    continue
+                if workQueue.empty():
+                    i += 1
+                    workQLock.release()
+                    continue
+                else:
+                    data = workQueue.get()
+                    workQLock.release()
+                    break
+                workQLock.release()
+            if data == []:
+                raise StopIteration()
+
         return data
 
     def rewind(self):
@@ -176,6 +254,10 @@ class Data(object):
             # Read the first two lines
             line = self.File.readline()
             line = self.File.readline()
+
+        if self.sourceFlag == 2:
+            # Stream in use, no possible to rewind
+            return
 
     def copy(self):
         """ Returns a copy of the iterator """
@@ -200,3 +282,10 @@ class Data(object):
         if self.dbConnection and self.dbCursor:
             self.dbCursor.close()
             self.dbConnection.close()
+        if self.stream and self.thread:
+            self.exitQLock.acquire()
+            self.exitQ.put(1)
+            self.exitQLock.release()
+            # Wait to thread to finish
+            t.sleep(1)
+            self.stream.close()
