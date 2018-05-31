ecc066f3401ecc5ad149b2cc881d03610f17bd91
Jose Septien <jaseptienh@gmail.com>
2018-05-31 16:18:51 -0500

    Add functions to draw labels and grid. Expand view area.

--- osciloscopeplot.py
+++ osciloscopeplot.py
@@ -23,6 +23,7 @@ class Osciloscope(oglC.OGLCanvas):
         self.data = []
         self.Range = [0, 0]
         self.varName = ""
+        self.numDivisions = 10
 
     def InitGL(self):
         glClearColor(0.9, 0.9, 0.9, 1)
@@ -30,7 +31,7 @@ class Osciloscope(oglC.OGLCanvas):
         #
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
-        glOrtho(0.0, 1.0, 0.0, 1.0, 1.0, 10.0)
+        glOrtho(-0.05, 1.01, -0.05, 1.05, 1.0, 10.0)
         #
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
@@ -41,21 +42,31 @@ class Osciloscope(oglC.OGLCanvas):
     def OnDraw(self):
         glClear(GL_COLOR_BUFFER_BIT)
 
-        # Draw osciloscope area
-        glPolygonMode(GL_FRONT, GL_FILL)
-        glColor3f(0.5, 0.5, 0.5, 1)
-        glRecti(0, 0, 1, 1)
-        for i in range(5):
-            glBegin(GL_LINES)
-            glVertex3f(0.0, 0.2 * (i + 1), 0)
-            glVertex3f(1, 0.2 * (i + 1), 0)
-            glVertex3f(0.2 * (i + 1), 0.0, 0.0)
-            glVertex3f(0.2 * (i + 1), 1.0, 0.0)
-            glEnd()
+        self.DrawGrid()
         #
         self.DrawData()
         #
+        self.DrawLabels()
         self.SwapBuffers()
+
+    def DrawGrid(self):
+        """ Draw the grid of the osciloscope """
+        # Draw osciloscope area
+        glPolygonMode(GL_FRONT, GL_LINE)
+        glColor3f(0.5, 0.5, 0.5, 1)
+        # Dotted line
+        glPushAttrib(GL_ENABLE_BIT)
+        glLineStipple(1, 0xAAAA)
+        glEnable(GL_LINE_STIPPLE)
+        glRecti(0, 0, 1, 1)
+        glBegin(GL_LINES)
+        for i in range(self.numDivisions):
+            glVertex3f(0.0, 0.1 * (i + 1), 0)
+            glVertex3f(1, 0.1 * (i + 1), 0)
+            glVertex3f(0.1 * (i + 1), 0.0, 0.0)
+            glVertex3f(0.1 * (i + 1), 1.0, 0.0)
+        glEnd()
+        glPopAttrib()
 
     def DrawData(self):
         """Draw the mean"""
@@ -64,10 +75,10 @@ class Osciloscope(oglC.OGLCanvas):
         y = (self.data[0] - self.Range[0]) / r
         glBegin(GL_LINE_STRIP)
         # Begin drawing the last value
-        glVertex3f(0.5, y, 0)
+        glVertex3f(0.99, y, 0)
         for i in range(1, len(self.data)):
             y = (self.data[i] - self.Range[0]) / r
-            glVertex3f(0.5 - (0.1 * i), y, 0)
+            glVertex3f(0.99 - (0.1 * i), y, 0)
         glEnd()
 
     def SetData(self, nData):
@@ -77,7 +88,7 @@ class Osciloscope(oglC.OGLCanvas):
         """
         if nData <= self.Range[0]:
             self.Range[0] = nData - 1
-        if nData <= self.Range[1]:
+        if nData >= self.Range[1]:
             self.Range[1] = nData + 1
         # Insert at front most recent value
         self.data.insert(0, nData)
@@ -91,3 +102,50 @@ class Osciloscope(oglC.OGLCanvas):
     def SetVariableName(self, varName):
         """ Set the name of the variable """
         self.varName = varName
+
+    def DrawLabels(self):
+        """ Draw the values of each axis and the name of the variable """
+        def GetLabelWidth(label):
+            """Returns the total width of the length of 'label', using the
+            fonts from glut"""
+            assert type(label) is str, "Incorrect type"
+
+            length = 0
+            for c in label:
+                length += glutBitmapWidth(GLUT_BITMAP_HELVETICA_18, ord(c))
+
+            assert type(length) is int
+            assert length >= 0
+
+            return length
+        #
+        def lerp(a, b, t):
+            """For interpolating between the range [a, b], according to the formula:
+            value = (1 - t) * a + t * b, for t in [0, 1]"""
+            assert 0.0 <= t <= 1.0
+            value = (1 - t) * a + t * b
+
+            assert a <= value <= b, "Out of range"
+            return value
+        #
+        # Draw the name of the variable
+        length = GetLabelWidth(self.varName)
+        if self.size:
+            length /= self.size.width
+        glColor3f(0.0, 0.0, 0.0)
+        glRasterPos2f(0.5 - length / 2, 1.015)
+        for c in self.varName:
+            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, ord(c))
+
+        # Draw the values of the range
+        divWidth = 1.0 / self.numDivisions
+        for i in range(self.numDivisions + 1):
+            x = lerp(self.Range[0], self.Range[1], i / self.numDivisions)
+            xLabel = '{:.1f}'.format(x)
+            length = GetLabelWidth(xLabel)
+            if self.size:
+                length /= self.size.width
+            glRasterPos2f(-0.04, i * divWidth)
+            for c in xLabel:
+                glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, ord(c))
+
