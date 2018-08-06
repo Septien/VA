960c44784553b9758d787de4c4428e1efb24bcd2
Jose Septien <jaseptienh@gmail.com>
2018-08-06 07:07:09 -0500

    Correct initialization on sort algorithm.

--- sort.py
+++ sort.py
@@ -16,22 +16,24 @@ def merge(A, p, q, r):
         L.append(A[p + i - 1])
     for j in range(n2):
         R.append(A[q + j])
-    L.append(float('inf'))
-    R.append(float('inf'))
+    L.append((-1, -float('inf')))
+    R.append((-1, -float('inf')))
     i, j = 0, 0
     for k in range(p, r):
-        if L[i][1] <= R[i][1]:
+        print(L[i])
+        print(R[i])
+        if L[i][1] >= R[i][1]:
             A[k] = L[i]
             i += 1
         else:
             A[k] = R[j]
             j += 1
 
-def sort(A. p, r):
+def sort(A, p, r):
     """ Sort A. A is an array containing tuppels with two elements. The first element 
     contains the key of the dict, and the second, its value."""
     if p < r:
-        q = int((p + q) / 2)
+        q = int((p + r) / 2)
         sort(A, p, q)
-        sort(A q + 1, r)
-        Merge(A, p, q, r)
+        sort(A, q + 1, r)
+        merge(A, p, q, r)
